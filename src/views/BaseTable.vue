595ad8d7de63ad3e9b1647591fd7d10590de0505
xuheliang <1431908773@qq.com>
2021-04-13 08:43:53 +0800

    commit code

---
+++ src/views/BaseTable.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="crumbs">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item>
+          <i class="el-icon-lx-cascades"></i> 基础表格
+        </el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="container">
+      <div class="handle-box">
+        <el-button type="primary" icon="el-icon-delete" class="handle-del mr10" @click="delAllSelection">
+          批量删除
+        </el-button>
+        <el-select v-model="query.address" placeholder="地址" class="handle-select mr10">
+          <el-option key="1" label="广东省" value="广东省"></el-option>
+          <el-option key="2" lable="湖南省" value="湖南省"></el-option>
+        </el-select>
+        <el-input v-model="query.name"></el-input>
+      </div>
+    </div>
+    <el-table :data="tableData" border class="table" ref="multipleTable" header-cell-class-name="table-header" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center"></el-table-column>
+      <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
+      <el-table-column prop="name" label="用户名"></el-table-column>
+      <el-table-column lable="账户余额">
+        <template slot-scope="scope">
+          ${{scope.row.money}}
+        </template>
+      </el-table-column>
+      <el-table-column label="头像(查看大图)" algin="center">
+        <template slot-scope="scope">
+          <el-img class="table-td-thumb" :src="scope.row.thumb" :preview-src-list="[scope.row.thumb]"></el-img>
+        </template>
+      </el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+      <el-table-column label="状态" align="center">
+        <template slot-scope="scope">
+          <el-tag :type="scope.row.state === '成功'?'success':(scope.row.state==='失败'?'danger':'')">{{scope.row.state}}</el-tag>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -65,17 +65,271 @@
             </el-card>
           </el-col>
         </el-row>
+        <el-card shadow="hover" style="height: 403px">
+          <div slot="header" class="clearfix">
+            <span>待办事项</span>
+            <el-button style="float: right;padding:3px 0" type="text">添加</el-button>
+          </div>
+          <el-table :show-header="false" :data="todolist" style="width:100%">
+            <el-table-column width="40">
+              <template slot-scope="scope">
+                <el-checkbox v-model="scope.row.status"></el-checkbox>
+              </template>
+            </el-table-column>
+            <el-table-column>
+              <template slot-scope="scope">
+                <div class="todo-item" :class="{'todo-item-del': scope.row.status}">
+                  {scope.row.title}
+                </div>
+              </template>
+            </el-table-column>
+            <el-table-column width="60">
+              <template>
+                <i class="el-icon-edit"></i>
+                <i class="el-icon-delete"></i>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <el-card shadow="hover">
+          <schart ref="bar" class="schart" canvasId="bar" :options="options"></schart>
+        </el-card>
+      </el-col>
+      <el-col :spam="12">
+        <el-card shadow="hover">
+          <schart ref="line" class="schart" canvasId="line" :options="options2"></schart>
+        </el-card>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
+import Schart from 'vue-schart'
 export default {
-
+  name: 'dashboard',
+  data() {
+    return {
+      name: localStorage.getItem('ms_username'),
+      todoList: [
+        {
+          title: '今天要修复100个bug',
+          status: false
+        },
+        {
+          title: '今天要修复100个bug',
+          status: false
+        },
+        {
+          title: '今天要写100行代码加几个bug',
+          status: false
+        },
+        {
+          title: '今天要修复100个bug',
+          status: false
+        },
+        {
+          title: '今天要修复100个bug',
+          status: true
+        },
+        {
+          title: '今天要写100行代码加几个bug吧',
+          status: true
+        }
+      ],
+      data: [
+        {
+          name: '2018/09/04',
+          value: 1083
+        },
+        {
+          name: '2018/09/05',
+          value: 941
+        },
+        {
+          name: '2018/09/06',
+          value: 1139
+        },
+        {
+          name: '2018/09/07',
+          value: 816
+        },
+        {
+          name: '2018/09/08',
+          value: 327
+        },
+        {
+          name: '2018/09/09',
+          value: 228
+        },
+        {
+          name: '2018/09/10',
+          value: 1065
+        }
+      ],
+      options: {
+        type: 'bar',
+        title: {
+            text: '最近一周各品类销售图'
+        },
+        xRorate: 25,
+        labels: ['周一', '周二', '周三', '周四', '周五'],
+        datasets: [
+            {
+                label: '家电',
+                data: [234, 278, 270, 190, 230]
+            },
+            {
+                label: '百货',
+                data: [164, 178, 190, 135, 160]
+            },
+            {
+                label: '食品',
+                data: [144, 198, 150, 235, 120]
+            }
+        ]
+      },
+      options2: {
+        type: 'line',
+        title: {
+            text: '最近几个月各品类销售趋势图'
+        },
+        labels: ['6月', '7月', '8月', '9月', '10月'],
+        datasets: [
+          {
+              label: '家电',
+              data: [234, 278, 270, 190, 230]
+          },
+          {
+              label: '百货',
+              data: [164, 178, 150, 135, 160]
+          },
+          {
+              label: '食品',
+              data: [74, 118, 200, 235, 90]
+          }
+        ]
+      }
+    }
+  },
+  components: {
+    Schart
+  },
+  computed: {
+    role() {
+      return this.name === 'admin'?'超级管理员':'普通管理员'
+    }
+  },
+  methods: {
+    changeDate() {
+      const now = new Date().getTime()
+      this.data.forEach((item,index) => {
+        const date = new Date(now - (6-index) * 86400 * 1000)
+        item.name = `${date.getFullYear()}/${date.getMonth+1}/${date.getDate()}}` 
+      })
+    }
+  }
 }
 </script>
 
 <style>
+.el-row {
+  margin-bottom: 20px;
+}
+.grid-content {
+  display: flex;
+  align-items: center;
+  height: 100px;
+}
+.grid-cont-right {
+  flex: 1;
+  text-align: center;
+  font-size:14px;
+  color: #999
+}
+.grid-num {
+  font-size: 30px;
+  font-weight: bold;
+}
+.grid-con-icon {
+  font-size: 50px;
+  width: 100px;
+  height: 100px;
+  text-align: center;
+  line-height: 100px;
+  color: #fff
+}
+.gird-con-1 .grid-con-icon {
+  background: rgb(45, 140,240);
+}
+.grid-con-1 .grid-num {
+  color: rgb(45,140,240)
+}
+.grid-con-2 .grid-con-icon {
+  background: rgb(100, 213, 114);
+}
+.grid-con-2 .grid-num {
+  color: rgb(45,140,240);
+}
+.grid-con-3 .grid-con-icon {
+  background: rgb(242, 94,67);
+}
+.grid-con-3 .grid-num {
+  color: rgb(242, 94 ,67)
+}
+
+.userInfo {
+  display: flex;
+  align-items: center;
+  padding-bottom: 20px;
+  border-bottom: 2px solid #ccc;
+  margin-bottom: 20px;
+}
+
+.user-avator {
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+}
+
+.user-info-cont {
+  padding-left: 50px;
+  flex: 1;
+  font-size: 14px;
+  color: #999;
+}
+
+.user-info-cont div:first-child {
+  font-size: 30px;
+  color: #222;
+}
+
+.usre-info-list {
+  font-size: 14px;
+  color: #999;
+  line-height: 25px;
+}
+
+.user-info-list span {
+  margin-left: 70px
+}
+
+.mgb20 {
+  margin-bottom: 20px;
+}
+
+.todo-item-del {
+  text-decoration: line-through;
+  color: #999;
+}
+
+.schart {
+  width: 100%;
+  height: 100%;
+}
 
 </style>
